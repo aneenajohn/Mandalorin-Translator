f7d91c484bf69bc895dfc5764c3fdda58379a614
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2020-12-10 19:46:58 +0530

    updated npm package

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ function clickHandler() {
         .then(response => response.json())
         .then(json => {
             var translatedAudio = json.contents.translated.audio;
+            console.log("output = "+translatedAudio);
             fs.writeFile('morse-output.wav', translatedAudio, (err) => {
                 // In case of a error throw err. 
                 if (err) throw err;
             })
+            console.log("The file was saved!");
             // outputDiv.innerText = translatedText;
             // console.log(translatedText);
             var translatedAudioElement = new Audio('morse-output.wav');
@@ -32,3 +34,10 @@ function getTranslationURL(text) {
     return serverURL + "?text=" + text + "&speed=7&tone=700";
 }
 
+// var fs = require('fs'); // reqire fileSystem node module
+// fs.writeFile("pathToFile", "Content", function(err) {
+//   if(err) {
+//     return console.log(err);
+//   }
+//   console.log("The file was saved!");
+// });
---
+++ bundle.js
@@ -0,0 +1,40 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+
+},{}],2:[function(require,module,exports){
+var txtInput = document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn-translate");
+var outputDiv = document.querySelector("#output")
+
+btnTranslate.addEventListener("click", clickHandler)
+
+
+const fs = require('browserify-fs');
+
+function clickHandler() {
+    var inputText = txtInput.value;
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedAudio = json.contents.translated.audio;
+            console.log(translatedAudio);
+            fs.writeFile('morse-output.wav', translatedAudio, (err) => {
+                // In case of a error throw err. 
+                if (err) throw err;
+            })
+            // outputDiv.innerText = translatedText;
+            // console.log(translatedText);
+            var translatedAudioElement = new Audio('morse-output.wav');
+            translatedAudioElement.play();
+        })
+}
+
+
+
+var serverURL = "http://api.funtranslations.com/translate/morse/audio.json"
+
+function getTranslationURL(text) {
+    return serverURL + "?text=" + text + "&speed=7&tone=700";
+}
+
+
+},{"fs":1}]},{},[2]);
