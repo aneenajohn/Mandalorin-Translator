b9a4b8e36d22910439b2e1ee5feb68d5086e10bb
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2020-12-10 14:39:51 +0530

    Updated js file

---
+++ app.js
@@ -0,0 +1,39 @@
+var txtInput = document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn-translate");
+var outputDiv = document.querySelector("#output")
+
+btnTranslate.addEventListener("click", clickHandler)
+
+mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+const fs = require('fs');
+
+function clickHandler() {
+    var inputText = txtInput.value;
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedAudio = json.contents.translated.audio;
+            fs.writeFile('morse-output.wav', translatedAudio, (err) => {
+                // In case of a error throw err. 
+                if (err) throw err;
+            })
+            // outputDiv.innerText = translatedText;
+            // console.log(translatedText);
+            var translatedAudioElement = new Audio('morse-output.wav');
+            translatedAudioElement.play();
+        })
+}
+
+
+
+var serverURL = "http://api.funtranslations.com/translate/morse/audio.json"
+
+function getTranslationURL(text) {
+    return serverURL + "?text=" + text + "&speed=7&tone=700";
+}
+
